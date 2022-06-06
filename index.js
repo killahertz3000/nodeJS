7070b7c8c14d4f5417a0a192b721b5a7846a55b4
Killa Hertz <killahertz3000@gmail.com>
2022-06-06 23:18:45 +0700

    lesson3

--- index.js
+++ index.js
@@ -1,107 +1,32 @@
-// node index.js hh:mm:ss-dd-mm-yyyy
+import fs from 'fs';
+import readline from 'readline';
+import { Stream } from 'stream';
+const TEST_FILE = './access_tmp.log';
+//const TRUE_FILE = './access.log';
+let IPs = process.argv.slice(2);
+const newStream = fs.createReadStream(TEST_FILE, 'utf-8');
+const outStream = new Stream();
+const readLineFromStream = readline.createInterface(newStream, outStream);
+const writeStreams = {};
 
- const { DateTime } = require('luxon');
- const EventEmitter = require('events');
+if(IPs.length == 0) {
+    IPs = ['89.123.1.41', '34.48.240.111'];
+}
 
- function getDateISOString(date) {
-   const [time, day, month, year] = date.split('-');
-   return `${year}-${month}-${day}T${time}`;
- }
+IPs.forEach((IP) => {
+    writeStreams[IP] = fs.createWriteStream(`${IP}_requests.log`, {
+        encoding: 'utf-8',
+        flags: 'a'
+    });
 
- function getDateDifference(date1, date2) {
-   const [start, end] = date1 > date2 ? [date2, date1] : [date1, date2];
-   return end
-     .diff(start, ['years', 'months', 'days', 'hours', 'minutes', 'seconds'])
-     .toObject();
- }
+});
 
- class Timer extends EventEmitter {
-   _currentTime = null;
+readLineFromStream.on('line', (line) => {
+    if(line.length == 0) { return; }
 
-   constructor(name, endTime) {
-     super();
-     this.name = name;
-     this.endTime = DateTime.fromISO(endTime);
-   }
-
-   start() {
-     this._timerObj = setInterval(() => {
-       this._currentTime = DateTime.now();
-       if (this.isEnd()) this.stop();
-       else this.tick();
-     }, 1000);
-   }
-
-   stop() {
-     clearInterval(this._timerObj);
-     this.emit('stop', {
-       [this.name]: `завершен.`,
-     });
-   }
-
-   tick() {
-     const { years, months, days, hours, minutes, seconds } = getDateDifference(
-       this.endTime,
-       this._currentTime
-     );
-     this.emit('tick', {
-       [this.name]:
-         (years > 0 ? `years: ${years}, ` : '') +
-         (months > 0 ? `months: ${months}, ` : '') +
-         (days > 0 ? `days: ${days}, ` : '') +
-         (hours < 10 ? `0${hours}:` : `${hours}:`) +
-         (minutes < 10 ? `0${minutes}:` : `${minutes}:`) +
-         (Math.round(seconds) < 10
-           ? `0${Math.round(seconds)}`
-           : `${Math.round(seconds)}`),
-     });
-   }
-
-   isEnd() {
-     return this._currentTime >= this.endTime;
-   }
- }
-
- class TimersList {
-   _timersList = [];
-   _timersState = {};
-
-   constructor(timeStringList) {
-     timeStringList.forEach((timeString, index) => {
-       const timer = new Timer(index + 1, timeString);
-
-       timer.on('tick', (tick) => {
-         this.updateState(tick);
-         this.renderState();
-       });
-       timer.on('stop', (stop) => {
-         this.updateState(stop);
-         this.renderState();
-       });
-
-       this._timersList.push(timer);
-     });
-   }
-
-   startTimers() {
-     this._timersList.forEach((timer) => timer.start());
-   }
-
-   updateState(newState) {
-     this._timersState = { ...this._timersState, ...newState };
-   }
-
-   renderState() {
-     console.clear();
-     Object.keys(this._timersState).forEach((timerName) => {
-       console.log(`Таймер ${timerName}: ${this._timersState[timerName]}`);
-     });
-   }
- }
-
- const timeList = process.argv
-   .splice(2)
-   .map((timeString) => getDateISOString(timeString));
- const timersList = new TimersList(timeList);
-
- timersList.startTimers();
+    IPs.forEach((IP) => {
+        if(line.indexOf(IP) !== -1) {
+            writeStreams[IP].write(line + '\n');
+        }
+    });
+});
